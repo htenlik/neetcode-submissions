bc94c8a27869e4f066ee1767e280c1c3cc154dff
Hüseyin Tenlik <62601638+htenlik@users.noreply.github.com>
2026-04-26 01:46:10 +0300

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int>dp(n+1, 0);
+        dp[1] = nums[0];
+        for(int i = 2; i <n; i++){
+            dp[i] = max(dp[i-1], nums[i-1]+dp[i-2]);
+        }
+
+        vector<int>dp2(n+1, 0);
+        dp2[1] = 0;
+        for(int i = 2; i <=n; i++){
+            dp2[i] = max(dp2[i-1], nums[i-1]+dp2[i-2]);
+        }
+        for(int x: dp) cout << x << " ";
+        cout << endl;
+        for(int x: dp2) cout << x << " ";
+
+        return max(dp[n-1],dp2[n]);
+    }
+};
