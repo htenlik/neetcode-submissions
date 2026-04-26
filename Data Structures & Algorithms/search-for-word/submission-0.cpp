1b680e2d22668c13c01cf56bcdcc2116709c74dd
Hüseyin Tenlik <62601638+htenlik@users.noreply.github.com>
2026-04-26 17:44:59 +0300

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,31 @@
+
+#include <queue>
+class Solution {
+   public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int pos) {
+        if (pos == word.size()) return true;
+
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) return false;
+
+        if (board[i][j] != word[pos]) return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        bool found = dfs(board, word, i + 1, j, pos + 1) || dfs(board, word, i - 1, j, pos + 1) ||
+                     dfs(board, word, i, j + 1, pos + 1) || dfs(board, word, i, j - 1, pos + 1);
+
+        board[i][j] = temp;
+
+        return found;
+    }
+};
