407bfc0d27fb00e2c3c08d6f126b4b2caac688f4
Hüseyin Tenlik <62601638+htenlik@users.noreply.github.com>
2026-04-26 03:02:35 +0300

    Add: decode-ways - submission-29

---
+++ Data Structures & Algorithms/decode-ways/submission-29.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+
+        for(int i = 2; i<=n; i++){
+            if (s[i - 1] != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if(s[i-2] == '1' ||(s[i-2]=='2' && (s[i-1]-'0')<7)){
+                dp[i] += dp[i - 2];
+            }
+        }
+        for(int x: dp) cout << x << " ";
+        return dp[n];
+
+    }
+};
